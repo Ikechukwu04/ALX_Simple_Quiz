4f9bdae7450a7647f1ec1a0381a108d5b8cd832b
Ikechukwu04 <163460439+Ikechukwu04@users.noreply.github.com>
2024-09-24 10:04:34 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -10,8 +10,8 @@ function checkAnswer() {
     const feedback = document.getElementById("feedback");
     
     // Check if an answer was selected
-    if (userAnswer) {
-        // Compare the user's answer with the correct answer
+       if (userAnswer) {
+        // Compare the value of the user's answer with the correct answer
         if (userAnswer.value === correctAnswer) {
             feedback.textContent = "Correct! Well done.";
             feedback.style.color = "green";
